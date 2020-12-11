43b29b12ffbdc1a9b16f2b683d24e54a20e20a61
rohan shedge <rohan_it05@hotmail.com>
2020-12-11 15:42:04 +0530

    Initial Commit

---
+++ src/main/java/com/rohan/Java_8_Snippets/App.java
@@ -0,0 +1,13 @@
+package com.rohan.Java_8_Snippets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
