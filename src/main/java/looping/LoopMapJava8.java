6962ccd305a5d4567c0328f1bf378752942f21ca
rohan shedge <rohan_it05@hotmail.com>
2020-12-18 12:07:31 +0530

    looping map through java 8

---
+++ src/main/java/looping/LoopMapJava8.java
@@ -0,0 +1,18 @@
+package looping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoopMapJava8 {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("A", 1);
+		map.put("B", 2);
+		map.put("C", 3);
+		map.put("D", 4);
+		map.put("E", 5);
+		
+		//Looping Map through Java8
+		map.forEach((k, v) -> System.out.println(k + " " + v));
+  }
+}
