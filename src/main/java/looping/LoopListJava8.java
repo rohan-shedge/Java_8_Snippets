ec87cb68292a6cbfd555478d187c75beb0deeff6
rohan shedge <rohan_it05@hotmail.com>
2020-12-21 19:34:49 +0530

    java.util.Function Consumer Usage

--- src/main/java/looping/LoopListJava8.java
+++ src/main/java/looping/LoopListJava8.java
@@ -3,15 +3,24 @@ import java.util.*;
 public class LoopListJava8 {
     
 public static void main(String[] args) {
-		 List<String> list = new ArrayList<>();
-	      list.add("A");
+		  List<String> list = new ArrayList<>();
+		  list.add("A");
 	      list.add("B");
 	      list.add("C");
 	      list.add("D");
 	      list.add("E");
+	      list.add(null);
 	      
 	      //Method References
 	      list.forEach(System.out::println);
+	      
+	      System.out.println("-----------------");
+	      
+	      //Print only Non null Element
+	      list.stream()
+	          .filter(Objects::nonNull)
+	          .forEach(System.out::println);
+	      
 	
     }
 }
