315fd4c7cd4c36c9fca4693510e7e4a39a18a1b1
rohan shedge <rohan_it05@hotmail.com>
2020-12-29 14:04:10 +0530

    formatting

--- src/main/java/com/rohan/java8/streambasics/StreamCreation.java
+++ src/main/java/com/rohan/java8/streambasics/StreamCreation.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 public class StreamCreation {
 	
 	public static void main(String[] args) {
-		String[] strArray = new String[] {"Rohan","pradnya","mugdha"};
+		String[] strArray = new String[] {"Rohan","pradnya","mugdha "};
 		
 		//Stream Creation from Arrays
 		//Stream contains ReferencePipeline.Head as Internal object, Stream is just a Reference
@@ -16,14 +16,17 @@ public class StreamCreation {
 		Stream anotherStream = Stream.of(strArray);
 
 		//print Stream
-		stream.forEach(x -> System.out.print(x));
+		stream.forEach(x -> System.out.print(x));       
+		System.out.println("");          
 		anotherStream.forEach(x -> System.out.print(x));
-		
+
+		System.out.println("");
 		//Convert to List and Print
 		List<String> list = Arrays.asList(strArray);
 		
+		System.out.println("");
 		//both are sequential
-		list.stream().forEach(x -> System.out.print(x));
+		list.stream().forEach(x -> System.out.print(x));       
 		list.forEach(x -> System.out.print(x));
 		
 	}
