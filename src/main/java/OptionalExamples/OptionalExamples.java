78f1a730c8236dcabe96dead5ee61741464f8a56
rohan shedge <rohan_it05@hotmail.com>
2021-01-08 12:01:00 +0530

    Optional

---
+++ src/main/java/OptionalExamples/OptionalExamples.java
@@ -0,0 +1,23 @@
+package OptionalExamples;
+import java.util.Optional;
+
+public class OptionalExamples {
+	public static void main(String[] args) {
+           
+		//Empty Optional
+		Optional<String> opt = Optional.empty();
+		Optional<String> opt1 = Optional.empty();
+		System.out.println(opt);
+		
+        // Use Optional.of when you are sure that the value is NON NULL
+		opt = Optional.of(null); 
+		
+		// Use Optional.ofNullable when you are not sure about the value
+		opt1 = Optional.ofNullable(null);
+		
+		//Use it in this way is live code
+		if (opt1.isPresent()) {
+			System.out.println(opt1.get());
+		}
+	}
+}
